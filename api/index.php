1e5effc679f3b15d94fa1b901bf92edbbe62e87a
fred2020nic <60634761+fred2020nic@users.noreply.github.com>
2023-04-02 23:15:12 -0600

    ultima-api-igual-vercel.json

--- api/index.php
+++ api/index.php
@@ -32,13 +32,18 @@ HERE;
 
 ?>
 
-<?php if ( $a==$b ) : ?>
+<?php header('X-PHP: test');
+http_response_code($_GET['code'] ?? 200);
 
+echo 'Test output';
 
-    <a href="../api/"></a>
-    <a href="../api/"></a>
-    <a href="../api/"></a>
-
-<?php endif ?>
+var_dump(file_get_contents('php://input'));
+var_dump($_GET);
+var_dump($_POST);
+var_dump($_SERVER);
+var_dump($_ENV);
+var_dump($_FILES);
+var_dump(opcache_get_status());
+ ?>
 
 

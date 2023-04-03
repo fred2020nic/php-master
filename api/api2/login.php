4c7ce6d79c2e160faa222c2dc9810dca7ef6748f
fred2020nic <60634761+fred2020nic@users.noreply.github.com>
2023-04-02 22:35:13 -0600

    api-subapi

---
+++ api/api2/login.php
@@ -0,0 +1,25 @@
+<?php
+// phpinfo();
+
+echo "hola mundo con php 7.4";
+
+echo "f2";
+
+echo "hola mundo con php 8.2";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="../api/"></a>
+</body>
+</html>
